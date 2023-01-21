651f74d3a246f0c16638a0b4230023f5d07f13e4
Harrsh Patel <dev@harrsh.com>
2023-01-21 14:08:57 +0530

    Tweaks

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { getOsEnv, getOsEnvOptional, normalizePort } from './libs/env';
+import { getOsEnv, getOsEnvOptional } from './libs/env';
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ const env = {
     node: getOsEnvOptional('APP_ENV') || 'local',
 
     app: {
-        port: normalizePort(getOsEnv('APP_PORT')) || getOsEnv('APP_PORT'),
+        port: getOsEnv('APP_PORT'),
         host: getOsEnv('APP_URL') || 'http://localhost:8000',
         root_dir: getOsEnv('APP_ENV') !== 'local' ? 'dist' : 'src',
         user_uploaded_content_path: getOsEnv('USER_UPLOADED_CONTENT_PATH'),
